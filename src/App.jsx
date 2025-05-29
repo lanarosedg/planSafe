f48158ea9655f37d21109f31b027bc12d72accde
lanarosedeguzman@gmail.com <lanarosedeguzman@gmail.com>
2025-05-29 16:05:35 +0800

    Modifications

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Logo/>
       <User/>
       <CurrentLocation/>
-      {/* <Search/> */}
+      <Search/>
     </div>
   )
 }
--- src/Search.jsx
+++ src/Search.jsx
@@ -2,7 +2,7 @@ function Search () {
     return (
         <>
         <div className="searchContainer">
-            <input type="text" className="searchInput" placeholder="Hi, {name}! Where are we gpoing today?" />
+            <input type="text" className="searchInput" placeholder="Hi, {name}! Where are we going today?" />
             <br />
             <button className="searchButton">Search</button>
         </div>
